609494062dcbcb85163c5b250c946dc34bed1e27
Gyadav1718 <86192597+Gyadav1718@users.noreply.github.com>
2021-07-13 20:44:05 +0530

    Update Rect1.js

--- Rect1.js
+++ Rect1.js
@@ -5,7 +5,7 @@ class Rect1{
      var options={
         restitution: 0 
      }
-      this.body=Bodies.rectangle(550,400,50,50,options)
+     this.body=Bodies.rectangle(400,610,90,50,{isStatic:true});
     this.width = 20;
     this.height = 100;
     
